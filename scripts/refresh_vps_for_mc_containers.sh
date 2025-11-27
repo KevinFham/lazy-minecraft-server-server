23ec0279e378a53d216e7d033e5f72eaee5a087d
KevinFham <kevin.pha01@gmail.com>
2025-11-26 23:56:32 -0600

    home rest api update

--- scripts/refresh_vps_for_mc_containers.sh
+++ scripts/refresh_vps_for_mc_containers.sh
@@ -6,10 +6,10 @@ DELAY=120
 
 while docker ps --format '{{.Names}}' | grep -qf $CONTAINER_WHITELIST; do
     sleep $DELAY
-    if [ "$(curl -X POST -H 'Content-Type: application/json' -d '{"action":"getVpsStatus"}' $VPS_REFRESH_ENDPOINT | jq -r '.code')" = "0" ] ; then
-        curl -X POST -H 'Content-Type: application/json' -d '{"action":"refreshVps"}' $VPS_REFRESH_ENDPOINT > /dev/null
+    if [ "$(curl -X GET $VPS_REFRESH_ENDPOINT | jq -r '.code')" = "0" ] ; then
+        curl -X PUT -H 'Content-Type: application/json' -d '{"action":"refreshVps"}' $VPS_REFRESH_ENDPOINT > /dev/null
     else
-        curl -X POST -H 'Content-Type: application/json' -d '{"action":"startVps"}' $VPS_REFRESH_ENDPOINT > /dev/null
+        curl -X PUT -H 'Content-Type: application/json' -d '{"action":"startVps"}' $VPS_REFRESH_ENDPOINT > /dev/null
     fi
 done
 
